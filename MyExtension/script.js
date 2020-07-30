fd7826d44a77933982ff4442b81f864aacbf9810
冯岳松 <yuesong-feng@foxmail.com>
2020-07-30 22:18:30 +0800

    Add files via upload

---
+++ MyExtension/script.js
@@ -0,0 +1,11 @@
+if(window.location.host=="www.google.com"){
+    document.getElementById("taw").style.display="none"; 
+    document.getElementById("bottomads").style.display="none"; 
+}
+// if(window.location.host=="www.baidu.com"){
+//     document.getElementById("3001").style.display="none"; 
+//     document.getElementById("3002").style.display="none"; 
+//     document.getElementById("3003").style.display="none"; 
+//     document.getElementById("3004").style.display="none"; 
+//     document.getElementById("3005").style.display="none"; 
+// }
