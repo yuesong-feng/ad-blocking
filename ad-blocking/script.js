ad82c67f15b56049042c11dfa9cce638d011471d
Yuesong-Feng <yuesong-feng@foxmail.com>
2020-08-01 19:53:59 +0800

    version 1.0

---
+++ ad-blocking/script.js
@@ -0,0 +1,25 @@
+var adds_opacity = 0.3;
+var url = window.location.host;
+switch(url){
+
+    case "www.google.com":
+        var adds = document.getElementsByClassName("ads-ad");
+        for(i=0;i<adds.length;i++){
+            adds[i].style.opacity=adds_opacity;
+        }
+    break;
+
+    case "www.baidu.com":
+        var id = 3001;
+        while(document.getElementById(String(id))!=null){
+            document.getElementById(String(id)).style.opacity=adds_opacity;
+            id++;
+        }
+    break;
+
+    case "www.youtube.com":
+        if(document.getElementById("player-ads")!=null){
+            document.getElementById("player-ads").style.opacity=adds_opacity;
+        }
+    break;
+}
