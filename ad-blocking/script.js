666cf6d0898dc3d92939bbdf821d94ac1822415e
Yuesong-Feng <yuesong-feng@foxmail.com>
2020-08-21 15:15:29 +0800

    代码整理

--- ad-blocking/script.js
+++ ad-blocking/script.js
@@ -1,39 +1,38 @@
 var adds_opacity = 0.3;
 var url = window.location.host;
-switch(url){
+switch (url) {
 
     case "www.google.com":
         var adds = document.getElementsByClassName("ads-ad");
-        for(let i=0;i<adds.length;i++){
-            adds[i].style.opacity=adds_opacity;
+        for (let i = 0; i < adds.length; i++) {
+            adds[i].style.opacity = adds_opacity;
         }
-        if(document.getElementById("taw")!=null){
-            document.getElementById("taw").style.opacity=adds_opacity;
+        if (document.getElementById("taw") != null) {
+            document.getElementById("taw").style.opacity = adds_opacity;
         }
-        if(document.getElementById("bottomads")!=null){
-            document.getElementById("bottomads").style.opacity=adds_opacity;
-
+        if (document.getElementById("bottomads") != null) {
+            document.getElementById("bottomads").style.opacity = adds_opacity;
         }
-    break;
+        break;
 
     case "www.baidu.com":
         var id = 3001;
-        while(document.getElementById(String(id))!=null){
-            document.getElementById(String(id)).style.opacity=adds_opacity;
+        while (document.getElementById(String(id)) != null) {
+            document.getElementById(String(id)).style.opacity = adds_opacity;
             id++;
         }
-    break;
+        break;
 
     case "www.youtube.com":
-        if(document.getElementById("player-ads")!=null){
-            document.getElementById("player-ads").style.opacity=adds_opacity;
+        if (document.getElementById("player-ads") != null) {
+            document.getElementById("player-ads").style.opacity = adds_opacity;
         }
-    break;
+        break;
 
     case "www.bbc.com":
         var adds = document.getElementsByClassName("nw-c-advert");
-        for(let i=0;i<adds.length;i++){
-            adds[i].style.opacity=adds_opacity;
+        for (let i = 0; i < adds.length; i++) {
+            adds[i].style.opacity = adds_opacity;
         }
-    break;
+        break;
 }
